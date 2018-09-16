e6f84d84a8f565e976267b8b0f496152610a79a6
Devanshu Saini <devanshu2@gmail.com>
2018-09-17 04:36:27 +0530

    restructure data

--- DevanshuTSystem/Home/HomeViewController.swift
+++ DevanshuTSystem/Home/HomeViewController.swift
@@ -20,10 +20,9 @@ class HomeViewController: BaseViewController {
     private var gridState = GridState.dual
     private lazy var searchBar = UISearchBar()
     private var isPhotoFetchCall = false
-    private var photos:[FlickrPhoto] = []
-    private var cellEdge: CGFloat = 0.0
-    private var padding: CGFloat = 0.0
     private var lastDataTask: URLSessionDataTask?
+    
+    private lazy var collectionModal = HomeCollectionModal()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +38,7 @@ class HomeViewController: BaseViewController {
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        self.calculateCellDimensions(size.width)
+        self.collectionModal.calculateCellDimensions(size.width)
         self.collectionView.reloadData()
     }
     
@@ -50,10 +49,11 @@ class HomeViewController: BaseViewController {
         self.searchBar.inputAccessoryView = self.doneToolbar
         self.searchBar.delegate = self
         self.searchBar.placeholder = "Search"
-        self.calculateCellDimensions(self.view.bounds.size.width)
+        self.collectionModal.delegate = self
+        self.collectionModal.calculateCellDimensions(self.view.bounds.size.width)
         self.collectionView.register(UINib(nibName: Constants.CellIdentifier.ImageCollectionViewCell, bundle: nil), forCellWithReuseIdentifier: Constants.CellIdentifier.ImageCollectionViewCell)
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
+        self.collectionView.delegate = self.collectionModal
+        self.collectionView.dataSource = self.collectionModal
         self.activityIndicator.isHidden = true
     }
     
@@ -61,16 +61,6 @@ class HomeViewController: BaseViewController {
         let rightBarButtonItem = UIBarButtonItem(image: self.gridState.getImage(), style: .plain, target: self, action: #selector(self.gridButtonAction(_:)))
         self.navigationItem.setRightBarButton(rightBarButtonItem, animated: true)
     }
-    
-    private func calculateCellDimensions(_ collectionWidth:CGFloat) {
-        let totalFillSpace = self.gridState.rawValue + 1
-        let blankSpace = collectionWidth - CGFloat( 10.0 * Double(totalFillSpace) )
-        let cellWidth = blankSpace/CGFloat(self.gridState.rawValue)
-        self.cellEdge = cellWidth
-        self.padding = (collectionWidth - ((cellWidth * CGFloat(self.gridState.rawValue)) + (CGFloat(10.0) * CGFloat(self.gridState.rawValue - 1))))/CGFloat(2) //more precise value
-        debugPrint("done")
-    }
-    
     
     override func toolBarDoneAction(_ sender: Any) {
         self.searchBar.resignFirstResponder()
@@ -82,21 +72,21 @@ class HomeViewController: BaseViewController {
         let duaAction = UIAlertAction(title: "Two In A Row", style: .default) { (action) in
             self.gridState = .dual
             self.updateGridbutton()
-            self.calculateCellDimensions(self.view.bounds.size.width)
+            self.collectionModal.calculateCellDimensions(self.view.bounds.size.width)
             self.collectionView.reloadData()
         }
         
         let tripleAction = UIAlertAction(title: "Three In A Row", style: .default) { (action) in
             self.gridState = .triple
             self.updateGridbutton()
-            self.calculateCellDimensions(self.view.bounds.size.width)
+            self.collectionModal.calculateCellDimensions(self.view.bounds.size.width)
             self.collectionView.reloadData()
         }
         
         let quadAction = UIAlertAction(title: "Four In A Row", style: .default) { (action) in
             self.gridState = .quad
             self.updateGridbutton()
-            self.calculateCellDimensions(self.view.bounds.size.width)
+            self.collectionModal.calculateCellDimensions(self.view.bounds.size.width)
             self.collectionView.reloadData()
         }
         
@@ -178,7 +168,7 @@ class HomeViewController: BaseViewController {
                 self.activityIndicator.isHidden = true
             }
             if response?.stat == .fail, let message = response?.message {
-                self.photos.removeAll()
+                self.collectionModal.photos.removeAll()
                 DispatchQueue.main.async {
                     let offset = CGPoint(x: -self.collectionView.adjustedContentInset.left,
                                          y: -self.collectionView.adjustedContentInset.top)
@@ -198,8 +188,8 @@ class HomeViewController: BaseViewController {
                 if let items = response?.photoResponse?.photos, items.count > 0 {
                     self.pageNumer = (response?.photoResponse?.page)!
                     if page == 1 {
-                        self.photos.removeAll()
-                        self.photos = items
+                        self.collectionModal.photos.removeAll()
+                        self.collectionModal.photos = items
                         DispatchQueue.main.async {
                             let offset = CGPoint(x: -self.collectionView.adjustedContentInset.left,
                                                  y: -self.collectionView.adjustedContentInset.top)
@@ -208,9 +198,9 @@ class HomeViewController: BaseViewController {
                         }
                     }
                     else {
-                        let oldCount = self.photos.count
-                        self.photos.append(contentsOf: items)
-                        let newCount = self.photos.count
+                        let oldCount = self.collectionModal.photos.count
+                        self.collectionModal.photos.append(contentsOf: items)
+                        let newCount = self.collectionModal.photos.count
                         var indexPaths = [IndexPath]()
                         for i in oldCount..<newCount {
                             indexPaths.append(IndexPath(item: i, section: 0))
@@ -244,6 +234,7 @@ class HomeViewController: BaseViewController {
         if(FlickrKit.shared.isAuthorized) {
             FlickrKit.shared.logout()
             self.userLoggedOut()
+            self.refreshData(with: true, PageNumber: 1)
         } else {
             FlickrKit.shared.logout()
             self.performSegue(withIdentifier: Constants.Segue.HomeToAuth, sender: self)
@@ -266,66 +257,20 @@ extension HomeViewController: UISearchBarDelegate {
     }
 }
 
-
-// MARK: - UICollectionViewDelegateFlowLayout Method
-extension HomeViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+extension HomeViewController: HomeCollectionModalDelegate {
+    func getGridState() -> GridState {
+        return self.gridState
     }
     
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if (indexPath.item + 1) == self.photos.count {
-            let newPage = self.pageNumer + 1
-            self.refreshData(with: false, PageNumber: newPage)
-            self.activityIndicator.isHidden = false
-        }
-        let photo = self.photos[indexPath.item]
-        let url = FlickrKit.shared.photoURL(ForSize: .small320, fromPhoto: photo)
-        ImageDownloader.shared.getImage(url: url)
-        let visibleIndexPaths = collectionView.indexPathsForVisibleItems
-        
-        var items:Set<String> = Set()
-        for ip in visibleIndexPaths {
-            let photoIP = self.photos[ip.item]
-            let urlIP = FlickrKit.shared.photoURL(ForSize: .small320, fromPhoto: photoIP)
-            items.insert(urlIP.absoluteString)
-        }
-        ImageDownloader.shared.setHighPriorityForURLS(items)
+    func getActivityIndicator() -> UIActivityIndicatorView {
+        return self.activityIndicator
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: self.cellEdge, height: self.cellEdge)
+    func getPageNumber() -> Int {
+        return self.pageNumer
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10.0
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10.0
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return .zero
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return .zero
-    }
-}
-
-// MARK: - UICollectionViewDataSource Method
-extension HomeViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.photos.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CellIdentifier.ImageCollectionViewCell, for: indexPath) as! ImageCollectionViewCell
-        let photo = self.photos[indexPath.item]
-        let url = FlickrKit.shared.photoURL(ForSize: .small320, fromPhoto: photo)
-        cell.imageView.setImage(with: url, placeHolder: #imageLiteral(resourceName: "gen-image"))
-        return cell
+    func refreshDataCall(with loader:Bool, PageNumber page:Int) {
+        self.refreshData(with: loader, PageNumber: page)
     }
 }
---
+++ DevanshuTSystem/Home/Modals/HomeCollectionModal.swift
@@ -0,0 +1,97 @@
+//
+//  HomeCollectionModal.swift
+//  DevanshuTSystem
+//
+//  Created by Devanshu Saini on 17/09/18.
+//  Copyright © 2018 Devanshu Saini. All rights reserved.
+//
+
+import UIKit
+
+protocol HomeCollectionModalDelegate: NSObjectProtocol {
+    func getGridState() -> GridState
+    func getActivityIndicator() -> UIActivityIndicatorView
+    func getPageNumber() -> Int
+    func refreshDataCall(with loader:Bool, PageNumber page:Int)
+}
+
+class HomeCollectionModal: NSObject {
+    public var photos:[FlickrPhoto] = []
+    private var cellEdge: CGFloat = 0.0
+    private var padding: CGFloat = 0.0
+    
+    public weak var delegate:HomeCollectionModalDelegate!
+    
+    public func calculateCellDimensions(_ collectionWidth:CGFloat) {
+        let totalFillSpace = self.delegate.getGridState().rawValue + 1
+        let blankSpace = collectionWidth - CGFloat( 10.0 * Double(totalFillSpace) )
+        let cellWidth = blankSpace/CGFloat(self.delegate.getGridState().rawValue)
+        self.cellEdge = cellWidth
+        self.padding = (collectionWidth - ((cellWidth * CGFloat(self.delegate.getGridState().rawValue)) + (CGFloat(10.0) * CGFloat(self.delegate.getGridState().rawValue - 1))))/CGFloat(2) //more precise value
+        debugPrint("done")
+    }
+}
+
+
+// MARK: - UICollectionViewDelegateFlowLayout Method
+extension HomeCollectionModal: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if (indexPath.item + 1) == self.photos.count {
+            let newPage = self.delegate.getPageNumber() + 1
+            self.delegate.refreshDataCall(with: false, PageNumber: newPage)
+            self.delegate.getActivityIndicator().isHidden = false
+        }
+        let photo = self.photos[indexPath.item]
+        let url = FlickrKit.shared.photoURL(ForSize: .small320, fromPhoto: photo)
+        ImageDownloader.shared.getImage(url: url)
+        let visibleIndexPaths = collectionView.indexPathsForVisibleItems
+        
+        var items:Set<String> = Set()
+        for ip in visibleIndexPaths {
+            let photoIP = self.photos[ip.item]
+            let urlIP = FlickrKit.shared.photoURL(ForSize: .small320, fromPhoto: photoIP)
+            items.insert(urlIP.absoluteString)
+        }
+        ImageDownloader.shared.setHighPriorityForURLS(items)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.cellEdge, height: self.cellEdge)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10.0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10.0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .zero
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return .zero
+    }
+}
+
+// MARK: - UICollectionViewDataSource Method
+extension HomeCollectionModal: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CellIdentifier.ImageCollectionViewCell, for: indexPath) as! ImageCollectionViewCell
+        let photo = self.photos[indexPath.item]
+        let url = FlickrKit.shared.photoURL(ForSize: .small320, fromPhoto: photo)
+        cell.imageView.setImage(with: url, placeHolder: #imageLiteral(resourceName: "gen-image"))
+        return cell
+    }
+}
